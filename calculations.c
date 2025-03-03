2132f3b8200f78f2d1a52e1c1f34e4f22a500b5c
JoeSemper <joe.semper77@gmail.com>
2025-03-03 16:51:01 +0300

    Functions and integrals

--- calculations.c
+++ calculations.c
@@ -1,2 +1,36 @@
 #include <stdio.h>
 #include "calculations.h"
+
+typedef float (*function)(float);
+
+float f1(float x)
+{
+    return 0.6 * x + 3;
+}
+
+float f2(float x)
+{
+    return (x - 2) * (x - 2) * (x - 2) - 1;
+}
+
+float f3(float x)
+{
+    return x / 3;
+}
+
+float df1(float x)
+{
+    return 3 / 5;
+}
+
+float df2(float x)
+{
+    return 3 * (x - 2) * (x - 2);
+}
+
+float df3(float x)
+{
+    return -(3 / (x * x));
+}
+
+
